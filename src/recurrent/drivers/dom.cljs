2ff69a9f65d9a77e60e9c41e05aa83971547af7c
Jeremy Kross <me@jeremykross.com>
2017-01-24 21:43:31 +0000

    ..

--- src/recurrent/drivers/dom.cljs
+++ src/recurrent/drivers/dom.cljs
@@ -24,40 +24,43 @@
   [parent]
   (fn [vtree$]
     (let [elem$ (e-sig/signal)]
-      (e-sig/subscribe-next 
+      (e-sig/subscribe
         (e-sig/sliding-slice 2 vtree$)
         (fn [[curr next]]
           (when (not= curr next)
-            (let [curr (vtree-for curr)
-                  next (vtree-for next)]
+            (let [curr-vtree (vtree-for curr)
+                  next-vtree (vtree-for next)]
               (when (and curr (not next))
-                (let [elem (js/virtualDom.create curr)]
+                (let [elem (js/virtualDom.create curr-vtree)]
                   (.appendChild parent elem)
                   (when (not (e-sig/is-completed elem$))
                     (e-sig/on-next elem$ elem))))
               (when (and curr next)
-                (let [patches (js/virtualDom.diff curr next)
+                (let [patches (js/virtualDom.diff curr-vtree next-vtree)
                       elem (js/virtualDom.patch @elem$ patches)]
-                  (when (not (e-sig/is-completed elem$))
-                    (e-sig/on-next elem$ elem))))))))
-              
-       (fn [selector]
-         (fn [event]
-           (let [event$ (e-sig/signal)
-                 elem-tap-$ (e-sig/tap elem$)]
-             (e-sig/subscribe-next 
-               elem-tap-$
-               (fn [elem]
-                 (e-sig/on-completed elem-tap-$)
-                 (let [selection 
-                       (if (= "root" (name selector))
-                         elem
-                         (.querySelector elem selector))]
-                   (.addEventListener selection
-                                      (name event)
-                                      (fn [e]
-                                        (e-sig/on-next event$ e))))))
-             event$))))))
+                  (e-sig/on-next elem$ elem))))))
+        (fn []
+          (js/alert "Removing!")
+          (.removeChild parent @elem$))
+        identity)
+
+      (fn [selector]
+        (fn [event]
+          (let [event$ (e-sig/signal)
+                elem-tap-$ (e-sig/tap elem$)]
+            (e-sig/subscribe-next 
+              elem-tap-$
+              (fn [elem]
+                (e-sig/on-completed elem-tap-$)
+                (let [selection 
+                      (if (= "root" (name selector))
+                        elem
+                        (.querySelector elem selector))]
+                  (.addEventListener selection
+                                     (name event)
+                                     (fn [e]
+                                       (e-sig/on-next event$ e))))))
+            event$))))))
 
 (defn from-id
   [id]
