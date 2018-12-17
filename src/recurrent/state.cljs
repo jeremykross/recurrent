eb8d8cc420d2d653e8daa59e6e801580ebefc92f
Jeremy Kross <me@jeremykross.com>
2018-12-16 19:39:47 -0500

    more work.

--- src/recurrent/state.cljs
+++ src/recurrent/state.cljs
@@ -1,11 +1,13 @@
 (ns recurrent.state
-  (:require [ulmus.signal :as ulmus]))
+  (:require
+    [clojure.set :as sets]
+    [ulmus.signal :as ulmus]))
 
 (defn isolate
-  [component scope]
+  [Component scope]
   (fn [props sources]
     (let [state-source-$ (:recurrent/state-$ sources)
-          sinks (component props (assoc sources
+          sinks (Component props (assoc sources
                                         :recurrent/state-$ (ulmus/map
                                                              (fn [state]
                                                                (get-in state scope))
@@ -37,6 +39,80 @@
           state-$ (ulmus/distinct (ulmus/reduce (fn [prev-state proc]
                                                   (proc prev-state))
                                                 nil (:recurrent/state-$ main-sinks)))]
-
       (ulmus/splice! state-placeholder-$ (ulmus/distinct state-$))
       main-sinks)))
+
+(defn index-for-key
+  [keyed-by k state]
+  (let [state-keys (map-indexed keyed-by state)]
+    (get
+      (into {}
+            (map-indexed (fn [idx k] [k idx]) state-keys))
+      k)))
+
+; need to attach child state to sources
+; and take child state out as a sink mapping it to it's correct index.
+(defn- make-child
+  [Component keyed-by k]
+  (fn [props sources]
+    (let [component-sinks
+          (Component props
+                     (assoc sources
+                            :recurrent/state-$
+                            (ulmus/map (fn [state]
+                                         (let [index (index-for-key keyed-by k state)]
+                                           (get state index)))
+                                       (:recurrent/state-$ sources))))]
+      (assoc component-sinks
+             :recurrent/state-$
+             (ulmus/map
+               (fn [reducer]
+                 (fn [state]
+                   (let [index (index-for-key keyed-by k state)]
+                     (assoc state index (reducer (get state index))))))
+               (:recurrent/state-$ component-sinks))))))
+
+
+
+    ; state-$ is the full array
+
+(defn collection-of
+  [Component & opts] 
+  (let [{:keys [collect keyed-by]
+         :or {keyed-by (fn [index child-state] (str index))
+              collect (fn [children-$] 
+                        {:recurrent/dom-$
+                         (ulmus/map
+                           (fn [children]
+                             `[:div {:class "array"} 
+                               ~@children])
+                           (ulmus/pickzip :recurrent/dom-$ children-$))})}}
+        (apply hash-map opts)]
+    (fn [props sources]
+      (let [children-$ (ulmus/distinct 
+                         (ulmus/reduce
+                           (fn [children state]
+                             (let [state-keys (set (map-indexed keyed-by state))
+                                   existing-keys (set (keys children))
+                                   new-keys (sets/difference state-keys existing-keys)
+                                   lost-keys (sets/difference existing-keys state-keys)]
+                               (with-meta 
+                                 (merge (apply dissoc children lost-keys)
+                                        (into {} (map (fn [k] [k ((make-child
+                                                                    Component
+                                                                    keyed-by
+                                                                    k) props sources)])
+                                                      new-keys)))
+                                 {:state state})))
+                           {}
+                           (:recurrent/state-$ sources)))
+            ordered-children-$ (ulmus/map
+                                 (fn [children]
+                                   (let [state (:state (meta children))
+                                         child-keys (map-indexed keyed-by state)]
+                                     (mapv #(get children %) child-keys)))
+                                 children-$)]
+        (collect ordered-children-$)))))
+                       
+
+
