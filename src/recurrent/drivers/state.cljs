db4cf9855be73d0bce9e7b9e0dfbab1610ddb623
Jeremy Kross <me@jeremykross.com>
2019-06-05 01:55:56 +0000

    various state fixes

--- src/recurrent/drivers/state.cljs
+++ src/recurrent/drivers/state.cljs
@@ -17,34 +17,44 @@
         (constantly state-$)))
     {:recurrent/driver? true}))
 
+(defn lens 
+  ([from Component] (lens from identity Component))
+  ([from to Component] (lens from to :recurrent/state-$ Component))
+  ([from to k Component]
+   (fn [sources & args]
+     (let [lensed-state-source-$
+           (constantly
+             (ulmus/map
+               from
+               ((get sources k))))
+           sinks (apply Component (assoc sources k lensed-state-source-$) args)
+           lensed-state-sink-$
+           (ulmus/map (fn [reducer]
+                        (fn [state] 
+                          (to
+                            state
+                            (reducer (from state)))))
+                      (get sinks k (ulmus/signal)))]
+       (assoc sinks k lensed-state-sink-$)))))
+
+
 (defn isolate
   ([scope Component] (isolate scope :recurrent/state-$ Component))
   ([scope k Component]
-   (fn [sources & args]
-     (let [isolated-state-source-$ (constantly (ulmus/map (fn [state] (get-in state scope)) ((get sources k))))
-           sinks (apply Component (assoc sources k isolated-state-source-$) args)
-           isolated-state-sink-$
-           (ulmus/map (fn [reducer]
-                        (fn [state] 
-                          (assoc-in 
-                            state
-                            scope
-                            (reducer (get-in state scope)))))
-                      (get sinks k (ulmus/signal)))]
-       (assoc sinks k isolated-state-sink-$)))))
+   (lens #(get-in % scope) #(assoc-in %1 scope %2) k Component)))
 
-(defn- index-for-key
+(defn- key->index
   [state key-fn k]
   (get
     (into
       {}
       (map-indexed
-        (fn [idx item] [(key-fn item) idx])
+        (fn [idx item] [(key-fn idx item) idx])
         state))
     k))
 
 (defn collection
-  ([Component] (collection Component identity))
+  ([Component] (collection Component (fn [index _] index)))
   ([Component key-fn]
    (fn [sources & args]
      (let [items-$
@@ -52,53 +62,58 @@
              (fn [state]
                (into 
                  {}
-                 (map (fn [item] [(key-fn item) item]) state)))
+                 (map-indexed (fn [index item] [(key-fn index item) item]) state)))
              ((:recurrent/state-$ sources)))
 
            elements-$
-           (ulmus/reduce
-             (fn [elements [gained lost]]
-               (-> 
-                 (apply disj elements (keys lost))
-                 (merge
-                   (into
-                     {}
-                     (map
-                       (fn [[k]]
-                         (let [component
-                               (apply
-                                 Component
-                                 (assoc
-                                   sources
-                                   :recurrent/state-$
-                                   (constantly (ulmus/map k items-$)))
-                                 args)]
-                           [k (assoc
-                                component
-                                :recurrent/state-$
-                                (ulmus/map
-                                  (fn [reducer]
-                                    (fn [state]
-                                      (let [idx (index-for-key
-                                                  state
-                                                  key-fn
-                                                  k)]
-                                        (assoc
-                                          state
-                                          idx
-                                          (reducer
-                                            (get
-                                              state
-                                              idx))))))
-                                  (or
-                                    (:recurrent/state-$ component)
-                                    (ulmus/signal))))]))
-                           gained)))))
-             {}
-             (ulmus/changed-keys items-$))
+           (ulmus/distinct
+             (ulmus/reduce
+               (fn [elements [gained lost]]
+                 (-> 
+                   (apply dissoc elements (keys lost))
+                   (merge
+                     (into
+                       {}
+                       (map
+                         (fn [[k]]
+                           (let [component
+                                 (apply
+                                   Component
+                                   (assoc
+                                     sources
+                                     :recurrent/state-$
+                                     (constantly (ulmus/map #(get % k) items-$)))
+                                   args)]
+                             [k (assoc
+                                  component
+                                  :recurrent/state-$
+                                  (ulmus/map
+                                    (fn [reducer]
+                                      (fn [state]
+                                        (let [idx (key->index
+                                                    state
+                                                    key-fn
+                                                    k)]
+                                          (assoc
+                                            state
+                                            idx
+                                            (reducer
+                                              (get
+                                                state
+                                                idx))))))
+                                    (or
+                                      (:recurrent/state-$ component)
+                                      (ulmus/signal))))]))
+                             gained)))))
+               {}
+               (ulmus/changed-keys items-$)))
            values-$ (ulmus/map vals elements-$)]
-       {:recurrent/state-$
-        (ulmus/pickmerge :recurrent/state-$ values-$)
+       {:elements-$
+        values-$
+
+        :recurrent/state-$
+        (ulmus/pickmerge :recurrent/state-$ values-$ {:replay? false})
+
         :recurrent/dom-$
         (ulmus/pickzip :recurrent/dom-$ values-$)}))))
 
